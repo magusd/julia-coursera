15ef46f7f2f5ea9d683b7736c5b94902b1caba2a
Lobs <vitor.lobs@gmail.com>
2020-10-10 23:23:24 -0300

    week1

---
+++ notebook-week1.jl
@@ -0,0 +1,75 @@
+### A Pluto.jl notebook ###
+# v0.12.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b1c7a67a-0b59-11eb-30d3-6b0f67c0b59a
+"hello moda"^3
+
+# ╔═╡ d22b7300-0b5a-11eb-2170-83aa8c21f51d
+
+
+# ╔═╡ 6364ec8a-0b5a-11eb-3b96-c9e9538c3214
+"Hello" * "modafoca"
+
+# ╔═╡ b65f0ab0-0b5a-11eb-2b8e-6dd3d2fd25f0
+"help println"
+
+# ╔═╡ 3210024a-0b5b-11eb-116a-794f9e4dd86d
+5040 / 60
+
+# ╔═╡ 0f44bed2-0b5f-11eb-069f-03f343031233
+3 & 5 > 0 
+
+# ╔═╡ 83dc75be-0b5f-11eb-2002-03f60107a33c
+8 & 5 > 0
+
+# ╔═╡ 86fce776-0b5f-11eb-360e-a7fdfb0cca47
+3 & 5
+
+# ╔═╡ 8cf3d6ba-0b5f-11eb-124d-d7d81075fbb4
+8 & 5
+
+# ╔═╡ 9022d5a2-0b5f-11eb-3a90-9152b777b2e9
+1 + 2 - 5 - 2 == 0 
+
+# ╔═╡ 1b5c9e46-0b5b-11eb-1396-3d2a5aebb28e
+3 | 4
+
+# ╔═╡ 3872d5b6-0b62-11eb-3be6-139e556e868d
+2 - true
+
+# ╔═╡ 0f78ddf4-0b5b-11eb-1738-4dafac804188
+1+3<5||2+2<1
+
+# ╔═╡ fd5886da-0b5a-11eb-231c-ad7e01907c20
+2^2^1^1.3^1.5^1.7^20
+
+# ╔═╡ b2c95ad6-0b5a-11eb-286d-c51e935e8799
+a = Array{Integer,2}(undef, 2, 2)
+
+# ╔═╡ ca3a0f9a-0b59-11eb-3cd8-1b8f43fe096f
+Array{Float64, 3}
+
+# ╔═╡ 71c2906e-0b68-11eb-24af-63b614b351bc
+
+
+# ╔═╡ Cell order:
+# ╠═b1c7a67a-0b59-11eb-30d3-6b0f67c0b59a
+# ╠═d22b7300-0b5a-11eb-2170-83aa8c21f51d
+# ╠═6364ec8a-0b5a-11eb-3b96-c9e9538c3214
+# ╠═b65f0ab0-0b5a-11eb-2b8e-6dd3d2fd25f0
+# ╠═3210024a-0b5b-11eb-116a-794f9e4dd86d
+# ╠═0f44bed2-0b5f-11eb-069f-03f343031233
+# ╠═83dc75be-0b5f-11eb-2002-03f60107a33c
+# ╠═86fce776-0b5f-11eb-360e-a7fdfb0cca47
+# ╠═8cf3d6ba-0b5f-11eb-124d-d7d81075fbb4
+# ╠═9022d5a2-0b5f-11eb-3a90-9152b777b2e9
+# ╠═1b5c9e46-0b5b-11eb-1396-3d2a5aebb28e
+# ╠═3872d5b6-0b62-11eb-3be6-139e556e868d
+# ╠═0f78ddf4-0b5b-11eb-1738-4dafac804188
+# ╠═fd5886da-0b5a-11eb-231c-ad7e01907c20
+# ╠═b2c95ad6-0b5a-11eb-286d-c51e935e8799
+# ╠═ca3a0f9a-0b59-11eb-3cd8-1b8f43fe096f
+# ╠═71c2906e-0b68-11eb-24af-63b614b351bc
--- pluto.jl
+++ pluto.jl
@@ -1,2 +1,2 @@
 using Pluto
-Pluto.run()
+Pluto.run(host="0.0.0.0")
